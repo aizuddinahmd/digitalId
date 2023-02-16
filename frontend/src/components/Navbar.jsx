de65ba63f9c01e366f3b798d6e3efa14ac6335df
Judin <aizuddinahmd0408@gmail.com>
2023-02-17 01:10:10 +0800

    adjust navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -12,12 +12,6 @@ const Navbar = () => {
 
   return (
     <div className={`w-full flex py-6 justify-between items-center navbar`}>
-      <img
-        src={toggle ? close : menu}
-        alt="menu"
-        className="w-[28px] h-[28-px] object-contain"
-        onClick={() => setToggle((prev) => !prev)}
-      />
       <Link to="/">
         <div className="w-[80px] h-[80px] rounded-full flex justify-center items-center cursor-pointer">
           <img
@@ -27,42 +21,62 @@ const Navbar = () => {
           />
         </div>
       </Link>
-      <div
-        className={`${
-          toggle ? "block" : "hidden"
-        } sm:block flex justify-center items-center gap-8`}
-      >
-        <Link to="/createId">
-          <ul className="list-none sm:flex hidden justify-end items-center flex-1">
-            <li
-              className={`font-epilogue font-normal cursor-pointer text-[16px] text-white`}
-            >
-              Create Profile
-            </li>
-          </ul>
+      <Link to="/createId">
+        <ul className="list-none sm:flex hidden justify-end items-center flex-1">
+          <li
+            className={`font-epilogue font-normal cursor-pointer text-[16px] text-white`}
+          >
+            Create Profile
+          </li>
+        </ul>
+      </Link>
+      <div className={`sm:flex hidden flex-row justify-end gap-4`}>
+        <CustomButton
+          btnType="button"
+          title={
+            address
+              ? `${address.slice(0, 8) + "..." + address.slice(-4)}`
+              : "Connect"
+          }
+          styles={address ? "bg-[#1c1c24]" : "bg-[#8c6dfd]"}
+          handleClick={() => {
+            connect();
+          }}
+        />
+        <Link to="/profile">
+          <div className="w-[52px] h-[52px] rounded-full bg-[#2c2f32] flex justify-center items-center cursor-pointer">
+            <img
+              src={pfp}
+              alt="user"
+              className="w-[100%] h-[100%] object-contain rounded-full "
+            />
+          </div>
         </Link>
-        <div className={`sm:flex hidden flex-row justify-end gap-4`}>
-          <CustomButton
-            btnType="button"
-            title={
-              address
-                ? `${address.slice(0, 8) + "..." + address.slice(-4)}`
-                : "Connect"
-            }
-            styles={address ? "bg-[#1c1c24]" : "bg-[#8c6dfd]"}
-            handleClick={() => {
-              connect();
-            }}
-          />
-          <Link to="/profile">
-            <div className="w-[52px] h-[52px] rounded-full bg-[#2c2f32] flex justify-center items-center cursor-pointer">
-              <img
-                src={pfp}
-                alt="user"
-                className="w-[100%] h-[100%] object-contain rounded-full "
-              />
-            </div>
-          </Link>
+      </div>
+      <div className="sm:hidden flex flex-1 justify-end items-center">
+        <img
+          src={toggle ? close : menu}
+          alt="menu"
+          className="w-[28px] h-[28-px] object-contain"
+          onClick={() => setToggle((prev) => !prev)}
+        />
+        <div
+          className={`${
+            toggle ? "flex" : "hidden"
+          } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
+        >
+          <div className="flex mx-4">
+            <Link to="/Profile">Profile</Link>
+            <CustomButton
+              btnType="button"
+              title={address ? "Create a campaign" : "Connect"}
+              styles={address ? "bg-[#1dc071]" : "bg-[#8c6dfd]"}
+              handleClick={() => {
+                if (address) navigate("create-campaign");
+                else connect();
+              }}
+            />
+          </div>
         </div>
       </div>
     </div>
