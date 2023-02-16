590bcf60c8e25b1e5afcf294ef0642655a942f9f
Judin <aizuddinahmd0408@gmail.com>
2023-02-17 01:13:44 +0800

    adjust navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -21,37 +21,39 @@ const Navbar = () => {
           />
         </div>
       </Link>
-      <Link to="/createId">
-        <ul className="list-none sm:flex hidden justify-end items-center flex-1">
-          <li
-            className={`font-epilogue font-normal cursor-pointer text-[16px] text-white`}
-          >
-            Create Profile
-          </li>
-        </ul>
-      </Link>
-      <div className={`sm:flex hidden flex-row justify-end gap-4`}>
-        <CustomButton
-          btnType="button"
-          title={
-            address
-              ? `${address.slice(0, 8) + "..." + address.slice(-4)}`
-              : "Connect"
-          }
-          styles={address ? "bg-[#1c1c24]" : "bg-[#8c6dfd]"}
-          handleClick={() => {
-            connect();
-          }}
-        />
-        <Link to="/profile">
-          <div className="w-[52px] h-[52px] rounded-full bg-[#2c2f32] flex justify-center items-center cursor-pointer">
-            <img
-              src={pfp}
-              alt="user"
-              className="w-[100%] h-[100%] object-contain rounded-full "
-            />
-          </div>
+      <div className="flex">
+        <Link to="/createId">
+          <ul className="list-none sm:flex hidden justify-end items-center flex-1">
+            <li
+              className={`font-epilogue font-normal cursor-pointer text-[16px] text-white`}
+            >
+              Create Profile
+            </li>
+          </ul>
         </Link>
+        <div className={`sm:flex hidden flex-row justify-end gap-4`}>
+          <CustomButton
+            btnType="button"
+            title={
+              address
+                ? `${address.slice(0, 8) + "..." + address.slice(-4)}`
+                : "Connect"
+            }
+            styles={address ? "bg-[#1c1c24]" : "bg-[#8c6dfd]"}
+            handleClick={() => {
+              connect();
+            }}
+          />
+          <Link to="/profile">
+            <div className="w-[52px] h-[52px] rounded-full bg-[#2c2f32] flex justify-center items-center cursor-pointer">
+              <img
+                src={pfp}
+                alt="user"
+                className="w-[100%] h-[100%] object-contain rounded-full "
+              />
+            </div>
+          </Link>
+        </div>
       </div>
       <div className="sm:hidden flex flex-1 justify-end items-center">
         <img
@@ -65,7 +67,7 @@ const Navbar = () => {
             toggle ? "flex" : "hidden"
           } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
         >
-          <div className="flex mx-4">
+          <div className="flex mx-4 flex-row">
             <Link to="/Profile">Profile</Link>
             <CustomButton
               btnType="button"
