5c6fa066adcb6dbdaf129d4ff6d81fa1450490a6
Judin <aizuddinahmd0408@gmail.com>
2023-02-17 01:14:44 +0800

    adjust navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -71,10 +71,10 @@ const Navbar = () => {
             <Link to="/Profile">Profile</Link>
             <CustomButton
               btnType="button"
-              title={address ? "Create a campaign" : "Connect"}
+              title={address ? "Create Id" : "Connect"}
               styles={address ? "bg-[#1dc071]" : "bg-[#8c6dfd]"}
               handleClick={() => {
-                if (address) navigate("create-campaign");
+                if (address) navigate("createId");
                 else connect();
               }}
             />
